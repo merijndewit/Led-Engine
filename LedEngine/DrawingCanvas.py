50309d063847b51d85ba6b9bb220c0bff62d702a
merijndewit <merijn.de.wit@gmail.com>
2022-03-17 14:04:35 +0000

    removed print()

--- LedEngine/DrawingCanvas.py
+++ LedEngine/DrawingCanvas.py
@@ -4,10 +4,8 @@ class DrawingCanvas(LedPanel):
 
     @classmethod
     def setPixel(cls, x, y, color):
-        print("set hex:", color)
         LedPanel.pixelArray[int(x)][int(y)] = color
         pixel = int(LedPanel.getPixelNumber(x, y))
-        print(cls.ledBrightness)
         LedPanel.pixels[pixel] = (int(color[1:3], 16) * (cls.Rpercentage / 100)*(cls.ledBrightness / 100), int(color[3:5], 16) * (cls.Gpercentage / 100)*(cls.ledBrightness / 100), int(color[5:7], 16) * (cls.Bpercentage / 100)*(cls.ledBrightness / 100))
         
         LedPanel.pixels.show()
