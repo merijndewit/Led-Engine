f1068a5af663cec6443cb484c0aa89a34712e5d1
merijndewit <merijn.de.wit@gmail.com>
2022-03-31 11:25:33 +0100

    added self and now works in new directory

--- LedEngine/modes/DisplayGif.py
+++ LedEngine/modes/DisplayGif.py
@@ -7,48 +7,49 @@ from LedPanel import LedPanel
 
 class DisplayGif(LedPanel):
     
-    gifUrl = ""
-    ledPanelsPixelWidth = 0
-    ledPanelsPixelHeight = 0
+    def __init__(self):
+        self.gifUrl = ""
+        self.ledPanelsPixelWidth = 0
+        self.ledPanelsPixelHeight = 0
 
     def UpdategifUrl(value):
         DisplayGif.gifUrl = str(value)
         print("gif set to:", value)
 
-    def ConvertGif():
-        if DisplayGif.gifUrl != "":
+    def ConvertGif(self):
+        if self.gifUrl != "":
             imageName = "tmp.gif"
-            path = os.path.dirname(os.path.realpath(__file__)) + "/tmpImages/" + imageName
+            path = os.path.dirname(os.path.realpath(__file__)) + "/../tmpImages/" + imageName
             try:
-                urllib.request.urlretrieve(DisplayGif.gifUrl, path)
+                urllib.request.urlretrieve(self.gifUrl, path)
             except:
                 print("coudn't download gif")
                 return
-            DisplayGif.resize_gif(path, None, (DisplayGif.ledPanelsPixelWidth, DisplayGif.ledPanelsPixelHeight))
+            self.resize_gif(self, path, None, (self.ledPanelsPixelWidth, self.ledPanelsPixelHeight))
             return path
 
     @classmethod
-    def PlayGif(cls):
-        DisplayGif.ledPanelsPixelWidth = LedPanel.ledPanelsPixelWidth
-        DisplayGif.ledPanelsPixelHeight = LedPanel.ledPanelsPixelHeight
-        print(DisplayGif.ledPanelsPixelWidth)
-        DisplayGif.ConvertGif()
-        gif = Image.open(os.path.dirname(os.path.realpath(__file__))+"/tmpImages/tmp.gif")
+    def PlayGif(self):
+        self.ledPanelsPixelWidth = self.ledPanelsPixelWidth
+        self.ledPanelsPixelHeight = self.ledPanelsPixelHeight
+        print(self.ledPanelsPixelWidth)
+        self.ConvertGif(self)
+        gif = Image.open(os.path.dirname(os.path.realpath(__file__))+"/../tmpImages/tmp.gif")
         
         while True:
             for i in range(gif.n_frames):
                 gif.seek(i)
                 rgb_im = gif.convert('RGB')
-                for y in range(DisplayGif.ledPanelsPixelHeight):
-                    for x in range(DisplayGif.ledPanelsPixelWidth):
-                        pixel = int(LedPanel.getPixelNumber(x, y))
+                for y in range(self.ledPanelsPixelHeight):
+                    for x in range(self.ledPanelsPixelWidth):
+                        pixel = int(self.getPixelNumber(x, y))
                         r, g, b = rgb_im.getpixel((x, y))  
-                        LedPanel.pixels[pixel] = (r * ((cls.Rpercentage / 100)*(cls.ledBrightness / 100)), g * (cls.Gpercentage / 100)*(cls.ledBrightness / 100), b * (cls.Bpercentage / 100)*(cls.ledBrightness / 100))
-                LedPanel.pixels.show()
+                        self.pixels[pixel] = (r * ((self.Rpercentage / 100)*(self.ledBrightness / 100)), g * (self.Gpercentage / 100)*(self.ledBrightness / 100), b * (self.Bpercentage / 100)*(self.ledBrightness / 100))
+                self.pixels.show()
                 time.sleep(0.2)
 
-    def resize_gif(path, save_as=None, resize_to=None):
-        all_frames = DisplayGif.extract_and_resize_frames(path, resize_to)
+    def resize_gif(self, path, save_as=None, resize_to=None):
+        all_frames = self.extract_and_resize_frames(self, path, resize_to)
 
         if not save_as:
             save_as = path
@@ -58,14 +59,13 @@ class DisplayGif(LedPanel):
         else:
             all_frames[0].save(save_as, optimize=True, save_all=True, append_images=all_frames[1:], loop=1000)
 
-    def extract_and_resize_frames(path, resize_to=None):
-        print(DisplayGif.ledPanelsPixelWidth)
+    def extract_and_resize_frames(self, path, resize_to=None):
         im = Image.open(path)
         all_frames = []
         try:
             while True:
                 im.convert('RGB')
-                new_frame = im.resize((DisplayGif.ledPanelsPixelWidth, DisplayGif.ledPanelsPixelHeight))
+                new_frame = im.resize((self.ledPanelsPixelWidth, self.ledPanelsPixelHeight))
                 all_frames.append(new_frame)
                 im.seek(im.tell() + 1)
         except EOFError:
