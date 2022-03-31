4fbc99576a6a908d14713117a7c3f1428d495d7e
merijndewit <merijn.de.wit@gmail.com>
2022-03-31 11:05:25 +0100

    added self in brians brain

--- LedEngine/modes/BriansBrain.py
+++ LedEngine/modes/BriansBrain.py
@@ -4,58 +4,60 @@ import time
 from LedPanel import LedPanel
 
 class BriansBrain(LedPanel):
-    grid = []
-    cols = 0
-    rows = 0
     
-    @classmethod
-    def Start(cls):
-        BriansBrain.cols = LedPanel.ledPanelsPixelWidth
-        BriansBrain.rows = LedPanel.ledPanelsPixelHeight
+    def __init__(self):
+        self.grid = []
+        self.cols = 0
+        self.rows = 0
 
-        listRow = [0] * BriansBrain.rows
+    @classmethod
+    def Start(self):
+        self.cols = self.ledPanelsPixelWidth
+        self.rows = self.ledPanelsPixelHeight
+
+        listRow = [0] * self.rows
         listCol = []
-        for i in range(BriansBrain.cols): 
-            for j in range(BriansBrain.rows):
+        for i in range(self.cols): 
+            for j in range(self.rows):
                 listRow[j] = int(random.randint(0, 2))
             listCol.append(listRow)
-            listRow = [0] * BriansBrain.cols
-        BriansBrain.grid = listCol
+            listRow = [0] * self.cols
+        self.grid = listCol
         while True:
-            BriansBrain.drawBriansBrain()
+            self.drawBriansBrain(self)
 
-    def drawBriansBrain():
-        for i in range(BriansBrain.cols):
-            for j in range(BriansBrain.rows):
-                pixel = LedPanel.getPixelNumber(j, i)
-                if (BriansBrain.grid[i][j] == 1):
-                    LedPanel.pixels[pixel] = (0, 1, 0)
-                elif(BriansBrain.grid[i][j] == 2):
-                    LedPanel.pixels[pixel] = (1, 0, 0)
+    def drawBriansBrain(self):
+        for i in range(self.cols):
+            for j in range(self.rows):
+                pixel = self.getPixelNumber(j, i)
+                if (self.grid[i][j] == 1):
+                    self.pixels[pixel] = (0, 1, 0)
+                elif(self.grid[i][j] == 2):
+                    self.pixels[pixel] = (1, 0, 0)
                 else:
-                    LedPanel.pixels[pixel] = (0, 0, 0)
-        LedPanel.pixels.show()
+                    self.pixels[pixel] = (0, 0, 0)
+        self.pixels.show()
         
-        nextGrid = BriansBrain.grid
-        for i in range(BriansBrain.cols):
-            for j in range(BriansBrain.rows):
-                if (BriansBrain.grid[i][j] == 2):
+        nextGrid = self.grid
+        for i in range(self.cols):
+            for j in range(self.rows):
+                if (self.grid[i][j] == 2):
                     nextGrid[i][j] = 1 #set the cell to dying state
-                elif (BriansBrain.grid[i][j] == 1):
+                elif (self.grid[i][j] == 1):
                     nextGrid[i][j] = 0
-                elif (BriansBrain.grid[i][j] == 0):
-                    neighbors = BriansBrain.countNeighborsBriansBrain(BriansBrain.grid, i, j)
+                elif (self.grid[i][j] == 0):
+                    neighbors = self.countNeighborsBriansBrain(self, self.grid, i, j)
                     if (neighbors == 2):
                         nextGrid[i][j] = 2
-        BriansBrain.grid = nextGrid
+        self.grid = nextGrid
         time.sleep(0.5)
 
-    def countNeighborsBriansBrain(grid, x, y):
+    def countNeighborsBriansBrain(self, grid, x, y):
         sum = 0
         for i in range(-1, 2):
             for j in range(-1, 2):
-                col = (x + i + BriansBrain.cols) % BriansBrain.cols
-                row = (y + j + BriansBrain.rows) % BriansBrain.rows
+                col = (x + i + self.cols) % self.cols
+                row = (y + j + self.rows) % self.rows
                 if (grid[col][row] == 2):
                     sum += 1
         return sum;
