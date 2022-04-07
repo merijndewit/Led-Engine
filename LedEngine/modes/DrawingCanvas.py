9723a62006ccca0d12e3ee7bd67ecda7212c5da3
merijndewit <merijn.de.wit@gmail.com>
2022-04-07 11:01:29 +0100

    DrawingCanvas now uses self. added super init

--- LedEngine/modes/DrawingCanvas.py
+++ LedEngine/modes/DrawingCanvas.py
@@ -2,13 +2,15 @@ from LedPanel import LedPanel
 
 class DrawingCanvas(LedPanel):
 
-    @classmethod
-    def setPixel(cls, aDict):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def setPixel(self, aDict):
         x = aDict.get("X")
         y = aDict.get("Y")
         color = aDict.get("color")
-        LedPanel.pixelArray[int(x)][int(y)] = color
-        pixel = int(LedPanel.getPixelNumber(x, y))
-        LedPanel.pixels[pixel] = (int(color[1:3], 16) * (cls.Rpercentage / 100)*(cls.ledBrightness / 100), int(color[3:5], 16) * (cls.Gpercentage / 100)*(cls.ledBrightness / 100), int(color[5:7], 16) * (cls.Bpercentage / 100)*(cls.ledBrightness / 100))
+        self.pixelArray[int(x)][int(y)] = color
+        pixel = int(self.getPixelNumber(x, y))
+        self.pixels[pixel] = (int(color[1:3], 16) * (self.Rpercentage / 100)*(self.ledBrightness / 100), int(color[3:5], 16) * (self.Gpercentage / 100)*(self.ledBrightness / 100), int(color[5:7], 16) * (self.Bpercentage / 100)*(self.ledBrightness / 100))
         
-        LedPanel.pixels.show()
+        self.pixels.show()
