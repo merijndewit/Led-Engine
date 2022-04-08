50a3a7caf90eafffd22071666b1c3bea78409c0d
merijndewit <merijn.de.wit@gmail.com>
2022-04-08 11:45:52 +0100

    WireWirld now uses self. added super init

--- LedEngine/modes/WireWorld.py
+++ LedEngine/modes/WireWorld.py
@@ -3,71 +3,77 @@ import time
 from LedPanel import LedPanel
 
 class WireWorld(LedPanel):
-    wireWorldGrid = []
-    wireWorldPixels = []
-    cols = 0
-    rows = 0
+    
+    def __init__(self) -> None:
+        super().__init__()
+    
+        self.wireWorldGrid = []
+        self.wireWorldPixels = []
+        self.cols = 0
+        self.rows = 0
 
     class wireWorldPixel:
         x = 0
         y = 0
-        more = 0
+        mode = 0
 
-    def setWireWorldPixel(x, y, mode):
-        wireWorldPixel = WireWorld.wireWorldPixel()
+    def setWireWorldPixel(self, aDict):
+        x = aDict.get("X")
+        y = aDict.get("Y")
+        mode = aDict.get("mode")
+        wireWorldPixel = self.wireWorldPixel()
         wireWorldPixel.x = x
         wireWorldPixel.y = y
         wireWorldPixel.mode = mode
-        WireWorld.wireWorldPixels.append(wireWorldPixel)
+        self.wireWorldPixels.append(wireWorldPixel)
 
-    @classmethod
-    def Start(cls):
-        WireWorld.wireWorldGrid = LedPanel.make2DArray(LedPanel.ledPanelsPixelWidth, LedPanel.ledPanelsPixelHeight)
-        for i in range(len(WireWorld.wireWorldPixels)):
-            WireWorld.wireWorldGrid[WireWorld.wireWorldPixels[i].x][WireWorld.wireWorldPixels[i].y] = WireWorld.wireWorldPixels[i].mode
+    def Start(self):
+        self.wireWorldGrid = self.make2DArray(self.ledPanelsPixelWidth, self.ledPanelsPixelHeight)
+        for i in range(len(self.wireWorldPixels)):
+            self.wireWorldGrid[self.wireWorldPixels[i].x][self.wireWorldPixels[i].y] = self.wireWorldPixels[i].mode
 
-        WireWorld.cols = LedPanel.ledPanelsPixelWidth
-        WireWorld.rows = LedPanel.ledPanelsPixelHeight
+        self.cols = self.ledPanelsPixelWidth
+        self.rows = self.ledPanelsPixelHeight
 
         while True:
-            WireWorld.RunWireWorld()
+            self.RunWireWorld()
 
-    def RunWireWorld():
-        for j in range(WireWorld.cols):
-            for i in range(WireWorld.rows):
-                pixel = LedPanel.getPixelNumber(j, i)
-                if (WireWorld.wireWorldGrid[j][i] == 0):
-                    LedPanel.pixels[pixel] = (0, 0, 0)
-                elif (WireWorld.wireWorldGrid[j][i] == 1):
-                    LedPanel.pixels[pixel] = (1, 1, 0)
-                elif (WireWorld.wireWorldGrid[j][i] == 2):
-                    LedPanel.pixels[pixel] = (0, 0, 1)
-                elif (WireWorld.wireWorldGrid[j][i] == 3):
-                    LedPanel.pixels[pixel] = (1, 0, 0)
-        nextgrid = LedPanel.make2DArray(WireWorld.cols, WireWorld.rows)
-        LedPanel.pixels.show()
+    def RunWireWorld(self):
+        for j in range(self.cols):
+            for i in range(self.rows):
+                pixel = self.getPixelNumber(j, i)
+                if (self.wireWorldGrid[j][i] == 0):
+                    self.pixels[pixel] = (0, 0, 0)
+                elif (self.wireWorldGrid[j][i] == 1):
+                    self.pixels[pixel] = (1, 1, 0)
+                elif (self.wireWorldGrid[j][i] == 2):
+                    self.pixels[pixel] = (0, 0, 1)
+                elif (self.wireWorldGrid[j][i] == 3):
+                    self.pixels[pixel] = (1, 0, 0)
+        nextgrid = self.make2DArray(self.cols, self.rows)
+        self.pixels.show()
         time.sleep(0.5)
         
-        for i in range(WireWorld.cols):
-            for j in range(WireWorld.rows):
-                if (WireWorld.wireWorldGrid[i][j] == 1):
-                    neighbors = WireWorld.countNeighborsWireWorld(WireWorld.wireWorldGrid, i, j)
+        for i in range(self.cols):
+            for j in range(self.rows):
+                if (self.wireWorldGrid[i][j] == 1):
+                    neighbors = self.countNeighborsWireWorld(self.wireWorldGrid, i, j)
                     if (neighbors == 1 or neighbors == 2):
                         nextgrid[i][j] = 2
                     else:
                         nextgrid[i][j] = 1
-                elif (WireWorld.wireWorldGrid[i][j] == 2):
+                elif (self.wireWorldGrid[i][j] == 2):
                     nextgrid[i][j] = 3
-                elif (WireWorld.wireWorldGrid[i][j] == 3):
+                elif (self.wireWorldGrid[i][j] == 3):
                     nextgrid[i][j] = 1
-        WireWorld.wireWorldGrid = nextgrid
+        self.wireWorldGrid = nextgrid
 
-    def countNeighborsWireWorld(wireWorldGrid, x, y):
-        sum = 0
+    def countNeighborsWireWorld(self, wireWorldGrid, x, y):
+        count = 0
         for i in range(-1, 2):
             for j in range(-1, 2):
-                col = (x + i + WireWorld.cols) % WireWorld.cols
-                row = (y + j + WireWorld.rows) % WireWorld.rows
+                col = (x + i + self.cols) % self.cols
+                row = (y + j + self.rows) % self.rows
                 if (wireWorldGrid[col][row] == 2):
-                    sum += 1
-        return sum;
+                    count += 1
+        return count
