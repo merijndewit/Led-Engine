d031eef04ba89c998fcc3604ef6142c4e147ade8
merijndewit <merijn.de.wit@gmail.com>
2022-04-26 13:07:03 +0100

    Fire now uses Color and new pixel_manager methods

--- LedEngine/modes/Fire.py
+++ LedEngine/modes/Fire.py
@@ -2,6 +2,7 @@ import random
 
 from LedStrip import LedStrip
 from pixel_manager import PixelManager
+from color import Color
 
 class Fire(LedStrip):
 
@@ -18,11 +19,12 @@ class Fire(LedStrip):
                 r = self.oneColorModeHex[0]*(pixelList[pixel] / 255)
                 g = self.oneColorModeHex[1]*(pixelList[pixel] / 255)
                 b = self.oneColorModeHex[2]*(pixelList[pixel] / 255)
-                PixelManager.Set_Pixel(pixel, (r, g, b), False)
+                col = Color(r, g, b)
+                PixelManager.set_color(col, pixel)
                 #self.neopixels[pixel] = ((self.oneColorModeHex[0]*(pixelList[pixel] / 255) * (self.Rpercentage / 100)*(self.ledBrightness / 100), self.oneColorModeHex[1] * (pixelList[pixel] / 255) * (self.Gpercentage / 100)*(self.ledBrightness / 100), self.oneColorModeHex[2] * (pixelList[pixel] / 255) * (self.Bpercentage / 100)*(self.ledBrightness / 100)))
                 if pixelList[pixel] <= 0:
                     pixelList[pixel] = 255
                 else:
                     pixelList[pixel] -= 1
-            PixelManager.Show_All()
+            PixelManager.show_all()
             
