0308299d02a3fc5235506b8468d95aecdf2b2259
merijndewit <merijn.de.wit@gmail.com>
2022-04-28 08:36:17 +0100

    now installs in s´the same directory as the install script

--- installLedEngine.sh
+++ installLedEngine.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 architecture=$(uname -m)
+dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
 echo "Installing LED-Engine."
-
 echo "Detected architecture: $architecture."
 
 arm7="armv7l"
 arm6="armv6l"
-
+cd $dir
 echo "installing nodejs for "$architecture""
 sudo wget https://nodejs.org/dist/v12.9.1/node-v12.9.1-linux-"$architecture".tar.xz
 sudo tar -xvf node-v12.9.1-linux-"$architecture".tar.xz
@@ -17,10 +17,9 @@ sudo rm -R node-v12.9.1-linux-"$architecture".tar.xz
 sudo rm -R node-v12.9.1-linux-"$architecture"
 testnode=$(node -v)
 echo "installed node: $testnode"
-#cd /
-#cd Led-Engine/
 sudo npm install --unsafe-perm
 sudo npm install socket.io
+sudo npm install formidable
 
 echo "Installing python dependencies."
 
@@ -35,4 +34,5 @@ sudo apt install libtiff5
 echo "Successfully installed LED-Engine!"
 echo "Type 'sh /Led-Engine/LedEngine/startLedEngine.sh' to start LED-Engine."
 echo "Enjoy using LED-Engine!"
+
 
